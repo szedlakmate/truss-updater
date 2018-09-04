17bbfd803a6f758ab4fe5590a32b7c8e77b8b615
Máté Szedlák <szedlakmate@gmail.com>
2018-09-05 01:36:59 +0200

    Add dirty calculate_stiffness_matrix() function

--- truss_objects.py
+++ truss_objects.py
@@ -7,6 +7,8 @@ Copyright MIT, Máté Szedlák 2016-2018.
 """
 
 from copy import deepcopy
+import itertools
+import math
 from read_input_file import read_structure_file
 
 
@@ -169,12 +171,83 @@ class Truss(object):
         self.start_model_updating()
 
     def calculate_stiffness_matrix(self, version):
-        print('Calculate %s stiffness matrix' % version)
-        pass
+        """
+        Stiffness matrix compilation
+        """
+        self.boundaries = list(k for k, _ in itertools.groupby(sorted(self.boundaries)))
+
+        # Setting known forces
+        known_f_a = []
+        known_f_not_zero = []
+        
+        for location in range(len(self['version'].node)):
+            known_f_a.append(location)
+            if self.loads.forces[location] != 0:
+                known_f_not_zero.append(location)
+                
+        for constraint in self.boundaries:  
+            if constraint[0] in known_f_a:
+                known_f_a.remove(constraint[0])
+                
+            if constraint[0] in known_f_not_zero:
+                known_f_not_zero.remove(constraint[0])
+
+        elements_lengths = [0] * len(self['version'].element)
+        _norm_stiff = [0] * len(self['version'].element)
+        _cx = [0] * len(self['version'].element)
+        _cy = [0] * len(self['version'].element)
+        _cz = [0] * len(self['version'].element)
+        _s_loc = [0] * len(self['version'].element)
+        local_stiffness_matrix = [0] * len(self['version'].element)
+        stiffness_matrix = [[0] * (len(self['version'].node) * 3)] * (len(self['version'].node) * 3)
+
+        for i in range(len(self['version'].element)):
+            elements_lengths[i] = \
+                math.sqrt(sum([(j - i) ** 2 for j, i
+                               in zip(self.nodal_coord[self.nodal_connections[i][1]],
+                                      self.nodal_coord[self.nodal_connections[i][0]])]))
+
+            _cx[i] = (self.nodal_coord[self.nodal_connections[i][1]][0]
+                           - self.nodal_coord[self.nodal_connections[i][0]][0]) / elements_lengths[i]
+            _cy[i] = (self.nodal_coord[self.nodal_connections[i][1]][1]
+                           - self.nodal_coord[self.nodal_connections[i][0]][1]) / elements_lengths[i]
+            _cz[i] = (self.nodal_coord[self.nodal_connections[i][1]][2]
+                           - self.nodal_coord[self.nodal_connections[i][0]][2]) / elements_lengths[i]
+            _norm_stiff[i] = self.elastic_modulo[i] / elements_lengths[i]
+
+            # local stiffness matrix calculation
+            _s_loc[i] = [
+                [_cx[i] ** 2, _cx[i] * _cy[i], _cx[i] * _cz[i], -_cx[i] ** 2,
+                 -_cx[i] * _cy[i], -_cx[i] * _cz[i]],
+                [_cx[i] * _cy[i], _cy[i] ** 2, _cy[i] * _cz[i], -_cx[i] * _cy[i],
+                 -_cy[i] ** 2, -_cy[i] * _cz[i]],
+                [_cx[i] * _cz[i], _cy[i] * _cz[i], _cz[i] ** 2, -_cx[i] * _cz[i],
+                 -_cy[i] * _cz[i], -_cz[i] ** 2],
+                [-_cx[i] ** 2, -_cx[i] * _cy[i], -_cx[i] * _cz[i], _cx[i] ** 2,
+                 _cx[i] * _cy[i], _cx[i] * _cz[i]],
+                [-_cx[i] * _cy[i], -_cy[i] ** 2, -_cy[i] * _cz[i], _cx[i] * _cy[i],
+                 _cy[i] ** 2, _cy[i] * _cz[i]],
+                [-_cx[i] * _cz[i], -_cy[i] * _cz[i], -_cz[i] ** 2, _cx[i] * _cz[i],
+                 _cy[i] * _cz[i], _cz[i] ** 2]]
+
+            local_stiffness_matrix[i] = [[y * self.cross_sectional_area_list[i] * _norm_stiff[i]
+                                          for y in x] for x in _s_loc[i]]
+
+            ele_dof_vec = self.element_dof[i]
+
+            stiffness_increment = [0] * (len(self['version'].node) * 3)
+
+            for j in range(3 * 2):
+                for k in range(3 * 2):
+                    stiffness_increment[ele_dof_vec[k]] = local_stiffness_matrix[i][j][k]
+                stiffness_matrix[ele_dof_vec[j]] = \
+                    [x + y for x, y in zip(stiffness_matrix[ele_dof_vec[j]], stiffness_increment)]
+
+        return stiffness_matrix
 
     def solve(self, version):
         # Calculate stiffness-matrix
-        self.calculate_stiffness_matrix(version)
+        stiffness_matrix = self.calculate_stiffness_matrix(version)
 
         print('Solve %s structure' % version)
 
