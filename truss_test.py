b56436d11d008b86602f0e6e84872db8c10eecd2
Máté Szedlák <szedlakmate@gmail.com>
2018-09-24 21:25:19 +0200

    Add some tests

--- truss_test.py
+++ truss_test.py
@@ -21,6 +21,7 @@ class TestClassInitializations(object):
         assert new_element.section == section
 
     def test_element_type_error(self, connection, material, section):
+        """Test element constructor"""
         with pytest.raises(TypeError):
             Element(connection, material, 0)
 
@@ -34,11 +35,12 @@ class TestClassInitializations(object):
             Element([0, 1.0], material, section)
 
     def test_element_invalid_connection(self, material, section):
+        """Test element constructor - connection"""
         with pytest.raises(ValueError):
             Element([0, 0], material, section)
 
-    """Test StructuralData class"""
     def test_structure(self, node_list, element_list):
+        """Test StructuralData class"""
         new_structure = StructuralData(node_list, element_list)
 
         assert new_structure.node == node_list
@@ -47,6 +49,7 @@ class TestClassInitializations(object):
         assert new_structure.element[0].section == element_list[0][2]
 
     def test_structure_type_error(self, node_list, element_list):
+        """Test StructuralData constructor"""
         with pytest.raises(TypeError):
             StructuralData([0.1], element_list)
 
@@ -60,18 +63,53 @@ class TestClassInitializations(object):
             StructuralData(node_list, [1.0])
 
 
+class TestStaticCalculations(object):
+    def test_element_length(self, BRIDGE):
+        """Test element length calculation"""
+        assert element_length(BRIDGE.original, 0) == 158.11388300841898
+
+    def test_error(self, bridge_displacement):
+        """Test error calculation"""
+        assert error([[0, 0.0]], bridge_displacement) == 0.0
+        assert error([[3, 0.0]], bridge_displacement) == 0.7777777777784746
+        assert error([[3, 0.7777777777784746]], bridge_displacement) == 0.0
+        assert error([[3, 0.0], [5, 2.0]], bridge_displacement) == math.sqrt(0.7777777777784746**2 + 2**2)
+
+
 class TestBridgeCalculations(object):
+    """Test stiffness matrix compilation"""
     def test_stiffness_matrix_calculation(self, BRIDGE_STIFFNESS_MATRIX):
-        Bridge = Truss('bridge.str', 'test', ['12Y'])
+        Bridge = Truss('bridge.str', 'test', ['11Y'])
         new_stiffness_matrix = calculate_stiffness_matrix(Bridge.original)
 
         assert new_stiffness_matrix == BRIDGE_STIFFNESS_MATRIX
 
-    def test_2D_struct_z_displacement(self):
-        Bridge = Truss('bridge.str', 'test', ['12Y'])
+    def test_2d_structural_z_displacement(self):
+        """Test whether 2D structures Z-displacement is blocked automatically"""
+        bridge = Truss('bridge.str', 'test', ['11Y'])
 
-        deformed = Bridge.solve(Bridge.original, Bridge.boundaries, Bridge.loads)
+        deformed = bridge.solve(bridge.original, bridge.boundaries, bridge.loads)
 
         z_total = sum([deformed['node'][x] if x % 3 == 2 else 0 for x in range(len(deformed['node']))])
 
         assert z_total == 0
+
+    def test_should_reset(self, BRIDGE):
+        """Test reset condition"""
+        BRIDGE.original.error = 0
+        BRIDGE.updated.error = 0
+        assert BRIDGE.should_reset() is False
+
+        BRIDGE.original.error = 1
+        BRIDGE.updated.error = 0
+        assert BRIDGE.should_reset() is False
+
+        BRIDGE.original.error = 0
+        BRIDGE.updated.error = 1
+        assert BRIDGE.should_reset() is True
+
+    def test_update_is_better(self, BRIDGE):
+        """Test first update for BRIDGE"""
+        BRIDGE.start_model_updating(1)
+        assert BRIDGE.should_reset() is False
+        assert BRIDGE.original.error > BRIDGE.updated.error
